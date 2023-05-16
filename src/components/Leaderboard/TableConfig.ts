ca4b27dc3a835049d8d413578b5d370fdf34b0e9
BUNGA <3010086711@qq.com>
2023-05-16 18:03:16 +0800

    git commit

---
+++ src/components/Leaderboard/TableConfig.ts
@@ -0,0 +1,94 @@
+export const config = new Map([
+  [
+    'school',
+    [
+      {
+        name: '学生姓名',
+        key: 'name',
+        order: 1,
+        noEllipsis: true,
+        textAlign: 'center',
+      },
+      {
+        name: '班级',
+        key: 'class',
+        order: 2,
+        noEllipsis: true,
+        textAlign: 'center',
+      },
+      {
+        name: '班主任',
+        key: 'classTeacher',
+        order: 3,
+        noEllipsis: true,
+        textAlign: 'center',
+      },
+      {
+        name: '等级',
+        key: 'level',
+        order: 4,
+        noEllipsis: true,
+        textAlign: 'center',
+      },
+    ],
+  ],
+  [
+    'class',
+    [
+      {
+        name: '班级',
+        key: 'class',
+        order: 1,
+        noEllipsis: true,
+        textAlign: 'center',
+      },
+      {
+        name: '班主任',
+        key: 'classTeacher',
+        order: 2,
+        noEllipsis: true,
+        textAlign: 'center',
+      },
+      {
+        name: '水滴数',
+        key: 'waterDropNum',
+        order: 3,
+        noEllipsis: true,
+        textAlign: 'center',
+      },
+    ],
+  ],
+  [
+    'progress',
+    [
+      {
+        name: '学生姓名',
+        key: 'name',
+        order: 1,
+        noEllipsis: true,
+        textAlign: 'center',
+      },
+      {
+        name: '班级',
+        key: 'class',
+        order: 2,
+        noEllipsis: true,
+        textAlign: 'center',
+      },
+      {
+        name: '班主任',
+        key: 'classTeacher',
+        order: 3,
+        noEllipsis: true,
+        textAlign: 'center',
+      },
+      {
+        name: '等级',
+        key: 'level',
+        order: 4,
+        noEllipsis: true,
+        textAlign: 'center',
+      },
+    ],
+  ],
+]);
